0b804784eede6b5eeee16fa1bcb58c82501c5663
Bruno Mateus <brunomateus@gmail.com>
2021-12-07 22:01:19 -0300

    Parsing CSV using synchronized func

--- app.ts
+++ app.ts
@@ -11,44 +11,39 @@ const fileServer = new Server('./public');
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
     const mangaService = new MangaService();
     
-    async function getAll(){
-        const mangas = await mangaService.getAll();
-        res.writeHead(200, {'Content-type': 'text/html'});
-        let content = "<tr>";
-        
-        for (let i in mangas) {
-            let line = `<td><img src="/img/one_piece/${path.basename(mangas[i].cover.toString())}" alt="${mangas[i].title}"><br>${mangas[i].volumeNumber} - ${mangas[i].title}<br><strong>Preço: R$${Number(mangas[i].price).toFixed(2)}</strong></td>`;
-
-
-            if((Number(i) + 1)  % 5 == 0) {
-                content += `${line}</tr><tr>`;
-            } else {
-                content += line;
-            }
-                
-        }
-
-        res.end(
-            `<html lang="pt-br">
-                <head>
-                    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-                </head>
-                <body>
-                    <table>
-                        <caption>Mangas</caption>
-                        <tbody>
-                        ${content}
-                        </tbody>
-                    </table>
-                </body>
-            </html>
-        `);
-    };
-
     if(req.url === '/') {
         if (req.method == 'GET') {
-            res.statusCode = 200;
-            getAll();
+            const mangas = mangaService.getAll();
+            res.writeHead(200, {'Content-type': 'text/html'});
+            let content = "<tr>";
+            
+            for (let i in mangas) {
+                let line = `<td><img src="/img/one_piece/${path.basename(mangas[i].cover.toString())}" alt="${mangas[i].title}"><br>${mangas[i].volumeNumber} - ${mangas[i].title}<br><strong>Preço: R$${Number(mangas[i].price).toFixed(2)}</strong></td>`;
+
+                if((Number(i) + 1)  % 5 == 0) {
+                    content += `${line}</tr><tr>`;
+                } else {
+                    content += line;
+                }
+                    
+            }
+
+            res.end(
+                `<html lang="pt-br">
+                    <head>
+                        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                    </head>
+                    <body>
+                        <table>
+                            <caption>Mangas</caption>
+                            <tbody>
+                            ${content}
+                            </tbody>
+                        </table>
+                    </body>
+                </html>
+            `);
+    
         }
     } else if (req.url?.startsWith('/img')) {
         fileServer.serve(req, res);
--- services/mangaService.ts
+++ services/mangaService.ts
@@ -8,20 +8,21 @@ const dbPath = path.join(__dirname, '../../db/mangas.csv');
 export class MangaService {
     private _mangas: Manga[] = [];
 
-    async getAll(): Promise<Manga[]> {
-        if (this._mangas.length == 0) {
-            const fielContent = await fs.promises.readFile(dbPath);
-            const records = parse(fielContent, {columns: true});
+    getAll(): Manga[]{
+        if(this._mangas.length == 0) {
+            const fileContent = fs.readFileSync(dbPath);
+            const records = parse(fileContent, {columns: true});
             for (let record of records) {
                 const title = record.title;
                 const volumeNumber = record.volumeNumber;
                 const cover = record['cover.href'];
                 const price = record.price;
+
                 const manga = new Manga(title, volumeNumber, "", [], cover, price);
                 this._mangas.push(manga);
-            }    
+            }
         }
         return this._mangas;
     }
-
 }
+
