2581f3c04be798f4a18f6f4492a5d5eb75c8940f
Bruno Mateus <brunomateus@gmail.com>
2022-06-22 18:29:32 -0300

    Adicionando lógica de remoção de arquivos existentes

--- strapi-backend/src/api/manga/controllers/manga.js
+++ strapi-backend/src/api/manga/controllers/manga.js
@@ -4,6 +4,38 @@
  *  manga controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { parseMultipartData } = require('@strapi/utils')
+const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController('api::manga.manga');
+const parseBody = ctx => {
+    if (ctx.is('multipart')) {
+      return parseMultipartData(ctx)
+    }
+  
+    const { data } = ctx.request.body || {}
+  
+    return { data }
+  }
+
+module.exports = createCoreController('api::manga.manga', ({ strapi }) => ({
+  async update (ctx) {
+    const { data, files } = parseBody(ctx)
+    const { id } = ctx.params
+
+    const sanitizedInputData = await this.sanitizeInput(data, ctx)
+
+    const entry = await strapi.entityService.findOne('api::manga.manga', id, {
+        populate: { cover: true },
+      });
+    
+    if (files['cover']) {
+        const file = await strapi.service('plugin::upload.upload').findOne(entry.cover.id );
+        if (file) {
+            await strapi.service('plugin::upload.upload').remove(file);
+        }
+    }
+
+    const response = await super.update(ctx);
+    return response;
+  }
+}))
