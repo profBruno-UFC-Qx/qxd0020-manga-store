d680d0b2734bf6ed771d311ffed443df73f3cabc
Bruno Mateus <brunomateus@gmail.com>
2022-06-21 22:58:39 -0300

    Corrigindo o tratamento de errors com status diferente de 404

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,12 +4,15 @@ import App from './App.vue'
 import { api } from './baseConfig'
 import { router } from './router'
 
+
 api.interceptors.response.use(response => response, error => {
     if(error.response.status === 404) {
         router.replace({
             name: 'notFound',
             params: { msg: error.response.data.error.message }
         })
+    } else {
+        return Promise.reject(error.response)
     }
 })
 
